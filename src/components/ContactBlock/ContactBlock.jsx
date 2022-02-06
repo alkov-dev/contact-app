d944b5d2fea99eab6c5c7beff49303e2f8757a73
alkov-dev <darkzoner@list.ru>
2022-02-06 11:48:56 +0300

    add

--- src/components/ContactBlock/ContactBlock.jsx
+++ src/components/ContactBlock/ContactBlock.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect, useRef } from "react";
 import "./ContactBlock.css"
 import pen from './icons/pen.svg'
 import basket from './icons/basket.svg'
+import avatar from './icons/avatar.svg'
 import axios from 'axios';
 import EditPage from '../EditPage';
 import { useNavigate } from "react-router-dom";
@@ -26,67 +27,83 @@ const ContactBlock = ({contact, contactsData, inputValue}) => {
     return (
         <>
             { userID === contact.ownerId && inputValue === '' && (
-                <div className='contactsBlock__container'>
-                        <div>
-                            <div>{contact.ownerId}</div>
-                            <div>{contact.name}</div>
-                            <div>{contact.phonenumber}</div>
-                            <div>{contact.email}</div>
-                        </div>
-                        <div className='icons__container'>
-                             <img onClick={() => handleEditClick(contact.id)} className='penSVG' src={pen} alt="" />
-                             <img onClick={() => handleDeleteContact(contact.id)} className='basketSVG' src={basket} alt="" />
-                             {/* <img className='basketSVG' src={basket} alt="" /> */}
-                        </div>
+            <div className='contactsBlock__container'>
+                <div className='description__block'>
+                    <div>
+                        <img className='avatarSVG' src={avatar} alt="" />
+                    </div>
+                    <div className='description'>
+                        <div className='description__name'>{contact.name}</div>
+                        <div>{contact.phonenumber}</div>
+                        <div>{contact.email}</div>
+                    </div>
                 </div>
+                <div className='icons__container'>
+                     <img onClick={() => handleEditClick(contact.id)} className='penSVG' src={pen} alt="" />
+                     <img onClick={() => handleDeleteContact(contact.id)} className='basketSVG' src={basket} alt="" />
+                     {/* <img className='basketSVG' src={basket} alt="" /> */}
+                </div>
+             </div>
             )}
                
             { userID === contact.ownerId && inputValue === contact.name && (
-                <div className='contactsBlock__container'>
-                        <div>
-                            <div>{contact.ownerId}</div>
-                            <div>{contact.name}</div>
-                            <div>{contact.phonenumber}</div>
-                            <div>{contact.email}</div>
-                        </div>
-                        <div className='icons__container'>
-                             <img onClick={() => handleEditClick(contact.id)} className='penSVG' src={pen} alt="" />
-                             <img onClick={() => handleDeleteContact(contact.id)} className='basketSVG' src={basket} alt="" />
-                             {/* <img className='basketSVG' src={basket} alt="" /> */}
-                        </div>
+            <div className='contactsBlock__container'>
+                <div className='description__block'>
+                    <div>
+                        <img className='avatarSVG' src={avatar} alt="" />
+                    </div>
+                    <div className='description'>
+                        <div className='description__name'>{contact.name}</div>
+                        <div>{contact.phonenumber}</div>
+                        <div>{contact.email}</div>
+                    </div>
                 </div>
+                <div className='icons__container'>
+                    <img onClick={() => handleEditClick(contact.id)} className='penSVG' src={pen} alt="" />
+                    <img onClick={() => handleDeleteContact(contact.id)} className='basketSVG' src={basket} alt="" />
+                    {/* <img className='basketSVG' src={basket} alt="" /> */}
+                </div>
+            </div>
             )}
 
             { userID === contact.ownerId && inputValue === contact.email && (
-                <div className='contactsBlock__container'>
-                        <div>
-                            <div>{contact.ownerId}</div>
-                            <div>{contact.name}</div>
-                            <div>{contact.phonenumber}</div>
-                            <div>{contact.email}</div>
-                        </div>
-                        <div className='icons__container'>
-                             <img onClick={() => handleEditClick(contact.id)} className='penSVG' src={pen} alt="" />
-                             <img onClick={() => handleDeleteContact(contact.id)} className='basketSVG' src={basket} alt="" />
-                             {/* <img className='basketSVG' src={basket} alt="" /> */}
-                        </div>
+            <div className='contactsBlock__container'>
+                <div className='description__block'>
+                    <div>
+                        <img className='avatarSVG' src={avatar} alt="" />
+                    </div>
+                    <div className='description'>
+                        <div className='description__name'>{contact.name}</div>
+                        <div>{contact.phonenumber}</div>
+                        <div>{contact.email}</div>
+                    </div>
                 </div>
+                <div className='icons__container'>
+                    <img onClick={() => handleEditClick(contact.id)} className='penSVG' src={pen} alt="" />
+                    <img onClick={() => handleDeleteContact(contact.id)} className='basketSVG' src={basket} alt="" />
+                    {/* <img className='basketSVG' src={basket} alt="" /> */}
+                </div>
+            </div>
             )}
 
             { userID === contact.ownerId && inputValue === contact.phonenumber && (
-                <div className='contactsBlock__container'>
-                        <div>
-                            <div>{contact.ownerId}</div>
-                            <div>{contact.name}</div>
-                            <div>{contact.phonenumber}</div>
-                            <div>{contact.email}</div>
-                        </div>
-                        <div className='icons__container'>
-                             <img onClick={() => handleEditClick(contact.id)} className='penSVG' src={pen} alt="" />
-                             <img onClick={() => handleDeleteContact(contact.id)} className='basketSVG' src={basket} alt="" />
-                             {/* <img className='basketSVG' src={basket} alt="" /> */}
-                        </div>
+            <div className='contactsBlock__container'>
+                <div className='description__block'>
+                    <div>
+                        <img className='avatarSVG' src={avatar} alt="" />
+                    </div>
+                    <div className='description'>
+                        <div className='description__name'>{contact.name}</div>
+                        <div>{contact.phonenumber}</div>
+                        <div>{contact.email}</div>
+                    </div>
                 </div>
+                <div className='icons__container'>
+                    <img onClick={() => handleEditClick(contact.id)} className='penSVG' src={pen} alt="" />
+                    <img onClick={() => handleDeleteContact(contact.id)} className='basketSVG' src={basket} alt="" />
+                    {/* <img className='basketSVG' src={basket} alt="" /> */}
+                </div>
+            </div>
             )}
  
         </>
